5bb788f0f934e98fe1c26f9fad18c2ea59b595a9
Dipshan Adhikari <dipshanadhikari1234@gmail.com>
2022-09-29 16:14:37 +0545

    Added top loading bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,31 @@
 import "../styles/globals.css"
 
+import { useEffect, useState } from "react"
+import { useRouter } from "next/router"
+
+import LoadingBar from "react-top-loading-bar"
 import Layout from "../components/Layout"
 
 function MyApp({ Component, pageProps }) {
+	const router = useRouter()
+
+	const [progress, setProgress] = useState(0)
+
+	useEffect(() => {
+		router.events.on("routeChangeStart", () => setProgress(25))
+		router.events.on("routeChangeComplete", () => setProgress(100))
+		router.events.on("routeChangeError", () => setProgress(0))
+	}, [])
+
 	return (
 		<Layout>
+			<LoadingBar
+				color="#f11946"
+				progress={progress}
+				onLoaderFinished={() => setProgress(0)}
+				height={3}
+			/>
+
 			<Component {...pageProps} />
 		</Layout>
 	)
